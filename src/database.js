8e2b591085d934c9c12126832de86e0dcb453f13
Ray <ray_punkeki@hotmail.com>
2025-07-09 01:54:59 -0500

    feat: update develop

--- src/database.js
+++ src/database.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 //entorne de produccion, cuando hablen los contenedores
-mongoose.connect("mongodb://localhost:27017/Uma_Database")
-//mongoose.connect("mongodb://database/Uma_Database")
-.then
-(
-    db => console.log("DB is connected to", db.connection.host)
-).catch
-(
-    err => console.error(err)
-);
+mongoose
+  .connect(
+    "mongodb+srv://raydelcarmen45:sJ7EPv3ZGPTf1emm@craftica-backend.dozreis.mongodb.net/craftica_backend_database?retryWrites=true&w=majority"
+  )
+  //mongoose.connect("mongodb://database/Uma_Database")
+  .then((db) => console.log("DB is connected to", db.connection.host))
+  .catch((err) => console.error(err));
 
 export default mongoose;
